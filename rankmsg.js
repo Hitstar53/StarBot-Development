dc086598c2ed5fe1b80446e102c9a8525e283a69
Hitstar53 <84141920+Hitstar53@users.noreply.github.com>
2021-06-02 23:44:32 +0530

    Update rankmsg.js

--- rankmsg.js
+++ rankmsg.js
@@ -18,3 +18,32 @@ $onlyBotPerms[mentioneveryone;managemessages;{description:I need permission \`MA
 $onlyPerms[manageserver;{description:You need \`MANAGE_SERVER\` permission}{color:ff2050}]
 $cooldown[5s;Please wait **%time%**]`
 })
+
+//New version
+module.exports = ({
+name: "setrankmsg",
+aliases: "rankmsg",
+usage: "setrankmsg <message>",
+code: `
+$if[$message==]
+$title[**You can use these variables to customise your message:**]
+$description[
+
+**{user.tag\\}** = $userTag
+**{user.mention\\}** = <@$authorID>
+**{level\\}** = 1
+**{exp\\}** = 25
+]
+$addField[**Current message is**;\`$getServerVar[rmsg]\`;no]
+$color[BLACK]
+
+$else
+$description[Level-Up message has been set to:
+\`$message\`]
+$color[BLACK]
+$setServerVar[rmsg;$message]
+$endif
+$onlyBotPerms[managemessages;{description:I need permission \`MANAGE_MESSAGES\`}{color:BLACK}]
+$onlyPerms[manageserver;{description:You need \`MANAGE_SERVER\` permission}{color:BLACK}]
+$cooldown[10s;Please wait **%time%** before using this command again!]`
+})
