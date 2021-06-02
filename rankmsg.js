f13759b65d781eb8cc87621bd4d90b7e1fba074e
Hitstar53 <84141920+Hitstar53@users.noreply.github.com>
2021-06-02 23:43:11 +0530

    create rankmsg.js for backup

---
+++ rankmsg.js
@@ -0,0 +1,20 @@
+module.exports = ({
+ name: "setrankmsg",
+ usage: "setrankmsg <message>",
+ code: `$description[You have been setted the message to:
+\`$message\`]
+$color[01ff00]
+$setServerVar[rmsg;$message]
+$onlyIf[$message!=;You can also use this variables:
+\`\`\`
+{user.tag} = $userTag
+{user.mention} = <@$authorID>
+{level} = 1
+{exp} = 25
+\`\`\`
+Current msg is:
+\`$getServerVar[rmsg]\`]
+$onlyBotPerms[mentioneveryone;managemessages;{description:I need permission \`MANAGE_MESSAGES\`/\`MENTION_EVERYONE\`}{color:ff2050}]
+$onlyPerms[manageserver;{description:You need \`MANAGE_SERVER\` permission}{color:ff2050}]
+$cooldown[5s;Please wait **%time%**]`
+})
