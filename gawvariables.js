6810e5dbfab8748222f7125e3c6a34f785f762d3
Hitstar53 <84141920+Hitstar53@users.noreply.github.com>
2021-06-06 10:33:39 +0530

    Update gawvariables.js

--- gawvariables.js
+++ gawvariables.js
@@ -1,11 +1,5 @@
 bot.variables({
- gtnchan: "",
- winnum: "",
- gtntries: "0",
- gtn: "false",
- gtnwins: "0",
- gtnattempts: "0",
- gtnstatus: "No ongoing game.",
- n1: "",
- n2: ""
+pene: "",
+timega: "",
+gaid: ""
 })
