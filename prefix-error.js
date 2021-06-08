d05c37eacfe4415f95f8b159af72f461a1d35883
Hitstar53 <84141920+Hitstar53@users.noreply.github.com>
2021-06-08 14:45:00 +0530

    Add files via upload

--- prefix-error.js
+++ prefix-error.js
@@ -1,6 +1,6 @@
 module.exports = ({
- name: "$alwaysExecute",
- code: `$channelSendMessage[$channelID;{title:Bruh}{description:This command doesnt exist!}{thumbnail:https://cdn.discordapp.com/emojis/830968952975917125.png?v=1}{color:RED}{delete:10s}]
+name: "$alwaysExecute",
+code: `$channelSendMessage[$channelID;{description:This command doesnt **exist!** ❌}{color:BLACK}{delete:10s}]
 $onlyIf[$commandinfo[$replaceText[$message[1];$getServerVar[prefix];];name]==;]
 $onlyIf[$stringStartsWith[$message;$getServerVar[prefix]]!=false;]`
 })
